3519b88eb81bf69104e743559b302d4eb3f96168
RODRIGO MIGUEL DA COSTA COLAÇO <a89828@ualg.pt>
2025-11-25 15:57:06 +0000

    exemplo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Rodrigo',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Site de exemplo do dokku',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
