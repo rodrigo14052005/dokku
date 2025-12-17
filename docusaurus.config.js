460cdfaf9b6e25c2fadc39702b988d1c77823d81
rodrigo14052005 <rodrigocolaco31@gmail.com>
2025-12-17 19:58:08 +0000

    teste

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/dokku',
+  baseUrl: '/loja',
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
